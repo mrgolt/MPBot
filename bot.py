583db558eeae9b44f4ccaa8817030e0fac324ca4
J Golt <mrgolt404@gmail.com>
2021-11-24 13:52:14 +0500

    win

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import math
 from get_position import *
 
 bot = telebot.TeleBot("2102905381:AAHbjtUofTgIvm0muTYZbbcTkeVSQlI5es4")
@@ -6,11 +7,13 @@ bot = telebot.TeleBot("2102905381:AAHbjtUofTgIvm0muTYZbbcTkeVSQlI5es4")
 
 @bot.message_handler(commands=["start"])  # Хендлер на команду start
 def start_message(message):
+    tg_analytic.statistics(message.chat.id, message.text)
     bot.send_message(message.chat.id, "Узнайте, на каком месте находиться ваш товар в поисковой выдачи на Wildberries.\nДля этого нужно ввести артикул и интересующий вас запрос.\nНапример: 43915761 контейнер для линз")
 
 
 @bot.message_handler(content_types=["text"])
 def message_handler(message):
+    tg_analytic.statistics(message.chat.id, message.text)
     data = str(message.text).split(" ")
     if len(data) > 1 and data[0].isnumeric():
         id = data[0]
@@ -22,7 +25,7 @@ def message_handler(message):
         if res is None:
             res = "Артикул "+id+" по запросу '"+key+"' на первых 20 страницах не обнаружен"
         else:
-            res = "Артикул находится на позиции "+str(res)
+            res = "Артикул находится на позиции "+str(res)+" на странице "+str(math.ceil(res/50))
         bot.send_message(message.chat.id,
                          res)
 
