df9acb24290b148c9a17769e864a4141238ff388
V <mrgolt>
2021-11-24 17:36:18 +0500

    update compose

--- init.py
+++ init.py
@@ -3,5 +3,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep # this should go at the top of the file
 from bs4 import BeautifulSoup
 import re
-import tg_analytic
+#import tg_analytic
 
