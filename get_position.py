10c6832f3fb28f643481c995dc0a4dbd4a245516
V <mrgolt>
2021-11-25 13:36:27 +0500

    update compose

--- get_position.py
+++ get_position.py
@@ -5,7 +5,8 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 #browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-browser = webdriver.Chrome("http://selenium:4444/wd/hub", options=options)
+browser = webdriver.Remote("http://selenium:4444/wd/hub", options=options)
+
 
 def get_keyword_position(limit: int=1000, query: str="", artikul: str=""):
     page = 1
